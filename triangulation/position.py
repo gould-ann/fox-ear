bff4d706135a5b318f89d9d076a5207bfa17103b
coltonls24 <coltonls24@gmail.com>
2018-03-23 23:27:02 -0500

    Classes for calulation and node object

---
+++ triangulation/position.py
@@ -0,0 +1,16 @@
+class Position:
+    nodes = []
+    maxInten = 0
+
+    def __init__(self, nodes):
+        maxNode = -1
+        self.nodes = nodes
+        for index, node in enumerate(self.nodes):
+            if node.getInten > self.maxInten
+            maxNode = index
+        
+        
+        self.nodes.pop(index)
+
+    def calculatePos(self):
+        for node in self.nodes
