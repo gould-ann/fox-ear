61a0aa3622a8c26cf8ca3aa2d8611c4e89ff6dac
anngould <anngould@iastate.edu>
2018-03-24 09:32:57 -0500

    added location algorithms

---
+++ node_scripts/get_location.php
@@ -0,0 +1,61 @@
+<?php
+
+// define the points
+$A = [0,0];
+$B = [3,3];
+$C = [10,1];
+$times = [3,2,1];
+
+// step one! calculate relative distances between time
+$time_ab = $times[0] / ($times[0] + $times[1]);
+$time_bc = $times[1] / ($times[1] + $times[2]);
+$time_ca = $times[2] / ($times[2] + $times[0]);
+// echo $time_bc;
+// step two! calculate the equations for the lines
+$eq_ab = [($B[1] - $A[1]) / ($B[0] - $A[0]), (($B[1] - $A[1]) / ($B[0] - $A[0])) * -1 * $A[0] + $A[1]];
+$eq_bc = [($C[1] - $B[1]) / ($C[0] - $B[0]), (($C[1] - $B[1]) / ($C[0] - $B[0])) * -1 * $B[0] + $B[1]];
+$eq_ca = [($A[1] - $C[1]) / ($A[0] - $C[0]), (($A[1] - $C[1]) / ($A[0] - $C[0])) * -1 * $C[0] + $C[1]];
+
+// echo json_encode($eq_bc);
+ // echo json_encode($eq_ca);
+// step three! calculate the points on that line
+$ab_x = ($B[0] - $A[0]) * $time_ab + $A[0];
+$bc_x = ($C[0] - $B[0]) * $time_bc + $B[0];
+$ca_x = ($A[0] - $C[0]) * $time_ca + $C[0];
+ // echo $ab_x;
+// echo $bc_x;
+// echo $ca_x
+
+
+//step four!
+
+$ab_y = $eq_ab[0] * $ab_x + $eq_ab[1];
+$bc_y = $eq_bc[0] * $bc_x + $eq_bc[1];
+$ca_y = $eq_ca[0] * $ca_x + $eq_ca[1];
+
+// echo $bc_y;
+// echo "<br>";
+// echo $bc_y;
+//echo $ca_y;
+$ab_yp = [(-1/$eq_ab[0]),(-1/$eq_ab[0]*(-$ab_x)) + $ab_y];
+$bc_yp = [(-1/$eq_bc[0]),(-1/$eq_bc[0]*(-$bc_x)) + $bc_y];
+$ca_yp = [(-1/$eq_ca[0]),(-1/$eq_ca[0]*(-$ca_x)) + $ca_y];
+
+// echo json_encode($b_yp);
+//step 5: set lines equal to each other :D
+
+$abp_bcp_x = ($ab_yp[1] - $bc_yp[1])/($bc_yp[0] - $ab_yp[0]);
+$bcp_cap_x = ($bc_yp[1] - $ca_yp[1])/($ca_yp[0] - $bc_yp[0]);
+$cap_abp_x = ($ca_yp[1] - $ab_yp[1])/($ab_yp[0] - $ca_yp[0]);
+
+$abp_bcp_y = $abp_bcp_x * ($ab_yp[0] + $ab_yp[1]);
+$bcp_cap_y = $bcp_cap_x * ($bc_yp[0] + $bc_yp[1]);
+$cap_abp_y = $cap_abp_x * ($ca_yp[0] + $ca_yp[1]);
+
+$final_x = ($abp_bcp_x + $bcp_cap_x + $cap_abp_x)/3;
+$final_y = ($abp_bcp_y + $bcp_cap_y + $cap_abp_y)/3;
+
+return [$final_x, $final_y];
+
+
+?>
